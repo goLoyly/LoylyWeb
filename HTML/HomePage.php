e6dbb26921e8c67b7be1e795a5e0a9728f4f2baa
goLoyly <31065744+goLoyly@users.noreply.github.com>
2017-08-16 14:46:40 +0300

    First upload

---
+++ HTML/HomePage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="../CSS/style.css">
+  <title>Loyly</title>
+</head>
+<body>
+<!----------------------changed---------------------------------------------->
+	<header>
+		<div><a href="">sign in</a> | <a href="">sign up</a></div>
+	</header>
+  <h1 class="title">Loyly</h1>
+  <div class="search_form">
+    <form>
+
+      <input type="text" name="massageSearchWhat" placeholder="What" id = "whatMassageDropDown">
+	  <input type="text" name="massageSearchWhen" placeholder="When" id = "whenMassageDropDown">
+      <input type="text" name="massageSearchWhere" placeholder="Where" id = "whereMassageDropDown">
+	  <input type="image" id="search_button" src="../IMGs/Apps-Search-icon.png" border="0" alt="Submit" height="30" width="30"/>
+    </form>
+	<!---------------------end-change--------------------->
+  </div>
+
+  <?php include "footer.php"; ?>
